8c6b414acbbe33fbbf22dc2576fbfd35c9e8060f
geia <111124706+geheyah@users.noreply.github.com>
2025-04-10 21:28:54 +0800

    added jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private PlayerCameraFollow cameraFollow;
     private PhysicsBody physicsBody;
 
+    public float jumpForce = 8f;
+    public float moveSpeed = 5f;
+    
     private void Start()
     {
         this.physicsBody = GetComponent<PhysicsBody>();
@@ -28,8 +31,18 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 moveDirection = (camForward * vertical + camRight * horizontal).normalized;
 
-        float moveSpeed = 5f;
-        this.physicsBody.velocity.x = moveDirection.x * moveSpeed;
-        this.physicsBody.velocity.z = moveDirection.z * moveSpeed;
+        float intendedMoveSpeed = moveSpeed;
+        if (physicsBody.isGrounded == PhysicsObjectState.Freefalling)
+        {
+            intendedMoveSpeed /= 2f;
+        }
+        
+        this.physicsBody.velocity.x = moveDirection.x * intendedMoveSpeed;
+        this.physicsBody.velocity.z = moveDirection.z * intendedMoveSpeed;
+        
+        if ((Input.GetKeyUp(KeyCode.Space)) && physicsBody.isGrounded == PhysicsObjectState.Grounded)
+        {
+            physicsBody.velocity.y += this.jumpForce;
+        }
     }
 }
